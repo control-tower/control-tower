ed8d341c16413c870f84ea4fafb86410007dd6c7
rrequero <rareq1987@gmail.com>
2016-08-30 13:32:25 +0200

    add dotenv

--- app/index-crons.js
+++ app/index-crons.js
@@ -1,5 +1,7 @@
 require('babel-core/register')({
     presets: [require.resolve('babel-preset-es2015-node5'), require.resolve('babel-preset-stage-3')],
+    ignore: /node_modules\/(?!ct-*)/,
 });
 
+require('dotenv').config({ silent: true });
 require('crons/index');
