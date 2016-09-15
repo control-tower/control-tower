8903a240a5994c9bf5025f45cdcdbc66e6463441
rrequero <rareq1987@gmail.com>
2016-09-15 18:17:10 +0200

    add sleep

--- app/src/crons/consul.cron.js
+++ app/src/crons/consul.cron.js
@@ -7,22 +7,27 @@ const crypto = require('crypto');
 
 let oldHashServices = null;
 
+
+function sleep(ms = 0) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
 async function tick() {
     try {
         logger.info('Executing tick in check consul microservice');
         let services = await ConsulService.getServices();
-        services = services.map((service) => (
-            {
-                active: true,
-                name: service.ServiceName,
-                url: `http://${service.ServiceAddress}:${service.ServicePort}`,
-                tags: service.ServiceTags,
-            }
-        ));
+        services = services.map((service) => ({
+            active: true,
+            name: service.ServiceName,
+            url: `http://${service.ServiceAddress}:${service.ServicePort}`,
+            tags: service.ServiceTags,
+        }));
         logger.debug('Checking if exist changes');
         if (crypto.createHash('md5').update(JSON.stringify(services)).digest('hex') !== oldHashServices) {
-            logger.info('Registering');
+            logger.info('Sleeping to wait run microservices');
             oldHashServices = crypto.createHash('md5').update(JSON.stringify(services)).digest('hex');
+            await sleep(30000);
+            logger.info('Registering microservices');
             await MicroserviceService.registerPackMicroservices(services);
         } else {
             logger.info('Not exist changes');
@@ -32,4 +37,4 @@ async function tick() {
     }
 }
 
-new CronJob('*/10 * * * * *', tick, null, true, 'America/Los_Angeles');  // eslint-disable-line no-new
+new CronJob('*/10 * * * * *', tick, null, true, 'America/Los_Angeles'); // eslint-disable-line no-new
