398800a0ba8164aafb6b096487ff91c991b7b024
Tiago Garcia <tiagojsag@gmail.com>
2018-11-14 15:40:41 +0100

    Improve error logging for ct

--- app/src/plugins/manageErrors.js
+++ app/src/plugins/manageErrors.js
@@ -10,28 +10,26 @@ function middleware(app, plugin) {
         try {
             await next();
         } catch (error) {
+            logger.error(error);
             ctx.status = error.status || 500;
+
             if (process.env.NODE_ENV === 'prod' && ctx.status === 500) {
                 if (plugin.config.jsonAPIErrors) {
                     ctx.response.type = 'application/vnd.api+json';
                     ctx.body = ErrorSerializer.serializeError(ctx.status, 'Unexpected error');
-                    return;
+                } else {
+                    ctx.body = 'Unexpected error';
                 }
-                ctx.body = 'Unexpected error';
                 return;
             }
+
             if (plugin.config.jsonAPIErrors) {
                 ctx.response.type = 'application/vnd.api+json';
                 ctx.body = ErrorSerializer.serializeError(ctx.status, error.message);
                 return;
             }
+
             ctx.response.type = 'application/json';
-            if (process.env.NODE_ENV !== 'prod') {
-                logger.error(error);
-            } else if (ctx.status === 500) {
-                ctx.body = 'Unexpected error';
-                return;
-            }
             ctx.body = {
                 error: error.message,
             };
