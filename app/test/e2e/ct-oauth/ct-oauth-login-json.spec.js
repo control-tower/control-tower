cf8225e33b1c6d5182ebd7b2c6fdaa222494097c
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-01-21 12:45:42 +0100

    Adds test for checking if user id exists

--- app/test/e2e/ct-oauth/ct-oauth-login-json.spec.js
+++ app/test/e2e/ct-oauth/ct-oauth-login-json.spec.js
@@ -163,6 +163,7 @@ describe('Auth endpoints tests', () => {
         response.redirects.should.be.an('array').and.length(0);
 
         const responseUser = response.body.data;
+        responseUser.should.have.property('id').and.be.an('string').and.not.be.empty;
         responseUser.should.have.property('email').and.equal('test@example.com');
         responseUser.should.have.property('role').and.equal('USER');
         responseUser.should.have.property('extraUserData').and.be.an('object');
